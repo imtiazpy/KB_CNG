2863da80fced8a5bf97269534c24b5477fe51919
imtiazpy <ahmed.imti1994@gmail.com>
2023-11-30 22:55:33 +0600

    changed settings for whitenose

--- kb_config/settings.py
+++ kb_config/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS += THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware", #To serve static assets in production
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
