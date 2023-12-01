02d88eb4020033a529e3e53193f21d284a3438ad
imtiazpy <ahmed.imti1994@gmail.com>
2023-12-01 22:55:12 +0600

    added ADMINS and EMAIL HOST settings

--- kb_config/settings.py
+++ kb_config/settings.py
@@ -177,3 +177,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/kbdash/'
 LOGIN_URL = '/auth/login/'
+
+ADMINS = [('Imtiaz Ahmed', 'imtiazahmed.py@gmail.com')]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env.str("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+
